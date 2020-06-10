a624e05845e63186d5495ca24a9cc60351ef0f93
Charlie Harding <charlie_harding@icloud.com>
2020-06-10 12:57:00 +0100

    Add scrollable map

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Map />
   </div>
 </template>
 
 <script lang="ts">
 import HelloWorld from './components/HelloWorld.vue';
+import Map from './components/Map.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    Map,
   },
 };
 </script>
@@ -21,8 +23,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import VueHead from 'vue-head';
+
 import App from './App.vue';
 
+Vue.use(VueHead);
 Vue.config.productionTip = false;
 
 new Vue({
